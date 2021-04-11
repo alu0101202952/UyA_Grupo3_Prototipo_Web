eda6f730fb2ceba700782446aae1a5ab50a49ccf
Andrea Calero Caro <47775111+alu0101202952@users.noreply.github.com>
2021-04-11 19:36:15 +0100

    Update ajax.js

--- js-ajax-firebase/ajax.js
+++ js-ajax-firebase/ajax.js
@@ -1,25 +1,49 @@
-$.ajax({
-url: ‘ruta’,
-type: ‘POST’,
-async: true,
-data: ‘parametro1=valor1&parametro2=valor2’.
-success: procesaRespuesta,
-error: muestraError})
-
-$( "#myForm" ).serialize();
-
-// Forma del formulario
-
-[
-    {
-        name: "campo1",
-        value: "valor1"
-    },
-    {
-        name: "campo2",
-        value: "valor2"
+function validaForm(){
+    // Campos de texto
+    if($("#numero").val() != int){  //Si es distinto a un numero entero
+        alert("El campo Numero debe ser un numero entero");
+        $("#numero").focus(); // Esta función coloca el foco de escritura del usuario en el campo Numero directamente.
+        return false;
     }
-]
+    if($("#nombre").val() == ""){
+        alert("El campo Nombre no puede estar vacío.");
+        $("#nombre").focus();       
+        return false;
+    }
+    if($("#apellidos").val() == ""){
+        alert("El campo Apellidos no puede estar vacío.");
+        $("#apellidos").focus();
+        return false;
+    }
+    if($("#direccion").val() == ""){
+        alert("El campo Dirección no puede estar vacío.");
+        $("#direccion").focus();
+        return false;
+    }
+
+    return true; // Si todo está correcto
+}
+
+
+$(document).ready(function () {
+  $("form").submit(function (event) {
+    var formData = {
+      name: $("#name").val(),
+      email: $("#email").val(),
+      superheroAlias: $("#superheroAlias").val(),
+    };
+    $.ajax({
+      type: "POST",
+      url: "process.php",
+      data: formData,
+      dataType: "json",
+      encode: true,
+    }).done(function (data) {
+      console.log(data);
+    });
+    event.preventDefault();
+  });
+});
 
 /** EJEMPLO DE FORMULARIO 1
  * // this is the id of the form
